de6b807a8aea76eb62a9c1443d923a497f9a02e9
souhardya69 <samantasouhardya@hotmail.com>
2021-03-03 17:21:17 +0530

    rest API adn routes refactoring

--- RestAPI_Mongoose/playground/promise-chaining-2.js
+++ RestAPI_Mongoose/playground/promise-chaining-2.js
@@ -1,13 +1,28 @@
 require('../src/db/mongoose')
+const { count } = require('../src/models/task')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('603d0d4028ded28dd76ded4d',{
-    isCompleted: true
-}).then((task)=>{
-    console.log(task)
-    return Task.countDocuments({isCompleted: false}).then((result)=>{
-        console.log(result)
-    })
+// Task.findByIdAndDelete('603d0d4028ded28dd76ded4d',{
+//     isCompleted: true
+// }).then((task)=>{
+//     console.log(task)
+//     return Task.countDocuments({isCompleted: false}).then((result)=>{
+//         console.log(result)
+//     })
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+const task = await Task.findByIdAndDelete(id)
+const count = await Task.countDocuments({
+    isCompleted: false
+})
+return count
+}
+
+deleteTaskAndCount('603e3e8931671b14ce476de6').then((count)=>{
+    console.log('No. of incomplete task ',count)
 }).catch((e)=>{
     console.log(e)
 })
--- RestAPI_Mongoose/playground/promise-chaining.js
+++ RestAPI_Mongoose/playground/promise-chaining.js
@@ -1,14 +1,31 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('603cb3323e96187f93eb8218',{
-    age: 1
-}).then((user)=>{
-    console.log(user)
-    return User.countDocuments({age: 1}).then((result)=>{
-        console.log(result)
-    })
+// User.findByIdAndUpdate('603cb3323e96187f93eb8218',{
+//     age: 1
+// }).then((user)=>{
+//     console.log(user)
+//     return User.countDocuments({age: 1}).then((result)=>{
+//         console.log(result)
+//     })
+// }).catch((e)=>{
+//     console.log(e)
+// })
+// //603ccb4f2ca737b0206f5fe7
+
+
+const updateAgeAndCount = async (id,age) =>{
+const user = await User.findByIdAndUpdate(id, {
+    age
+})
+const count = await User.countDocuments({
+    age
+})
+return count
+}
+
+updateAgeAndCount('603d0cdf28ded28dd76ded4b',1).then((count)=>{
+    console.log(count)
 }).catch((e)=>{
     console.log(e)
 })
-//603ccb4f2ca737b0206f5fe7
